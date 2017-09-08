0f6b7a5e188e626877999e3bb69d4d687e7733c1
Stephen Engelman <stephen.engelman@gmail.com>
2017-09-08 14:39:49 -0700

    cmake and cpp and demo changes

--- inc/vdm_proto.h
+++ inc/vdm_proto.h
@@ -1,12 +1,22 @@
 #include "systemc.h"
 #include "simple_target_socket.h"
+#include "simple_initiator_socket.h"
+#include <vector>
 
+using namespace std;
 using namespace sc_core;
 using namespace tlm_utils;
+using namespace tlm;
 
 class vdm_proto : public sc_module
 {
     public:
     vdm_proto(sc_module_name name);
+    void b_transport(tlm_generic_payload & trans, sc_time & delay);
 
+    simple_target_socket<vdm_proto> tsocket;
+    simple_initiator_socket<vdm_proto> isocket;
+
+    sc_event start;
+    vector<unsigned char> data_store;
 };
--- src/vdm_proto.cpp
+++ src/vdm_proto.cpp
@@ -1,5 +1,25 @@
 #include "vdm_proto.h"
 
-vdm_proto::vdm_proto(sc_module_name name) {
-    
+vdm_proto::vdm_proto(sc_module_name name) : sc_module(name)
+                                          , tsocket("tsocket")
+                                          , isocket("tsocket") {
+    tsocket.register_b_transport(this, &vdm_proto::b_transport);
+}
+
+void vdm_proto::b_transport(tlm_generic_payload & trans, sc_time & delay) {
+    while (true) {
+        wait(start);
+        if (trans.is_read()) { // READ
+
+        } else { // WRITE
+            for (int i = 0; i < trans.get_data_length(); i++) {
+                data_store.clear();
+                data_store.resize(trans.get_data_length());
+                data_store.push_back((trans.get_data_ptr())[i]);
+            }
+        }
+
+
+    }
+
 }
