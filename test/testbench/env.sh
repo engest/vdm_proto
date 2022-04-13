54eb0f4bfd1137f118bbefb4e23c9c92718718c6
Stephen Engelman <softwarerd@gmail.com>
2022-04-13 09:44:55 -0700

    buid fix, ld env, cm cleanup

---
+++ test/testbench/env.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH=/home/engest/devel/systemc/local/lib-linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/lib/Debug:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/test/tlmdriver/lib/Debug:$LD_LIBRARY_PATH
