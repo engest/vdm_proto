f45e6ba8bea534c10dc5fbf2797f2e5801f9961b
Stephen Engelman <softwarerd@gmail.com>
2022-04-13 22:03:12 -0700

    removed vector and start event

--- inc/vdm_proto.h
+++ inc/vdm_proto.h
@@ -1,7 +1,9 @@
+#ifndef __VDM_PROTO_H__
+#define __VDM_PROTO_H__
+
 #include "systemc.h"
 #include "simple_target_socket.h"
 #include "simple_initiator_socket.h"
-#include <vector>
 
 using namespace std;
 using namespace sc_core;
@@ -10,12 +12,11 @@ using namespace tlm;
 
 class vdm_proto : public sc_module
 {
-    public:
+public:
     vdm_proto(sc_module_name name);
     void b_transport(tlm_generic_payload & trans, sc_time & delay);
 
     simple_target_socket<vdm_proto> tsocket;
 
-    sc_event start;
-    vector<unsigned char> data_store;
 };
+#endif
