6bd9c0c0e865b1ca79b76147bf8532c5a86f8a27
Stephen Engelman <softwarerd@gmail.com>
2022-04-13 22:07:33 -0700

    Added isocket start_of_simulation, drive func

--- test/tlmdriver/src/tlmdriver.cpp
+++ test/tlmdriver/src/tlmdriver.cpp
@@ -1,9 +1,14 @@
 #include "tlmdriver.h"
 
-tlmdriver::tlmdriver(sc_module_name name) {
+tlmdriver::tlmdriver(sc_module_name name) : isocket("isocket") {
     SC_HAS_PROCESS(tlmdriver);
     SC_THREAD(drive);
+    cout << "Greatings from driver\n";
 }
+
+void tlmdriver::start_of_simulation() {
+}
+
 void tlmdriver::drive() {
     while (true)
     {
@@ -11,7 +16,11 @@ void tlmdriver::drive() {
 
         // Send GP
 
+        cout << "Driving time:" << sc_time_stamp() << "\n";
+        sc_time delay = sc_time(10, SC_NS);
         // Wait a time
+        wait(delay);
+        // trigger.notify(sc_time(10, SC_NS));
     }
     
 }
