8e7d5a32cd39cad6c8b66f7cd139d99b14941c88
Stephen Engelman <softwarerd@gmail.com>
2022-04-13 22:05:50 -0700

    added bdg cout

--- test/testbench/src/vdm_main.cpp
+++ test/testbench/src/vdm_main.cpp
@@ -3,6 +3,8 @@ using namespace sc_core;
 int sc_main(int argc, char * argv[]) {
     // Instance top
     testbench tb("tb");
+    cout << "Pre sc_start()\n";
     sc_core::sc_start();
+    cout << "Post sc_start()\n";
     return 0;
 }
