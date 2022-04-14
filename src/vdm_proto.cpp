6d76a520cb047e8e12b3f5ac3215086629fa2080
Stephen Engelman <softwarerd@gmail.com>
2022-04-13 22:04:10 -0700

    no change

--- src/vdm_proto.cpp
+++ src/vdm_proto.cpp
@@ -1,30 +1,16 @@
 #include "vdm_proto.h"
 
 vdm_proto::vdm_proto(sc_module_name name) : sc_module(name)
-                                          , tsocket("tsocket") {
+                                          , tsocket("tsocket")
+{
     tsocket.register_b_transport(this, &vdm_proto::b_transport);
+    cout << "Greatings from vdm_proto\n";
 }
 
 void vdm_proto::b_transport(tlm_generic_payload & trans, sc_time & delay) {
-    while (true) {
-        wait(start);
-        if (trans.is_read()) { // READ
-
-        } else { // WRITE
-            for (int i = 0; i < trans.get_data_length(); i++) {
-                data_store.clear();
-                data_store.resize(trans.get_data_length());
-                data_store.push_back((trans.get_data_ptr())[i]);
-            }
-        }
+    if (trans.is_read()) { // READ
+        cout << "VDM received READ transation";
+    } else { // WRITE
+        cout << "VDM received WRITE transation";
     }
-}
-
-void decode_data(unsigned char * data, unsigned int len) {
-    unsigned int header_len = 0;
-    unsigned int footer_len = 0;
-    unsigned int payload_len = 0;
-    if (len > 0)
-        header_len = data[0];
-    unsigned char header[header_len];
 }
