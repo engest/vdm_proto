c9d8e7756c99464515659b8de6884204cf2f3787
Stephen Engelman <softwarerd@gmail.com>
2022-04-15 14:07:05 -0700

    Moved instances to be members, not func vars

--- test/testbench/inc/testbench.h
+++ test/testbench/inc/testbench.h
@@ -4,9 +4,14 @@
 #include "systemc.h"
 #include "tlm.h"
 
+#include "vdm_proto.h"
+#include "tlmdriver.h"
+
 class testbench : public sc_module
 {
 public:
+    vdm_proto device;
+    tlmdriver driver;
     testbench(sc_module_name name);
     
 };
--- test/testbench/src/testbench.cpp
+++ test/testbench/src/testbench.cpp
@@ -1,11 +1,9 @@
 #include "testbench.h"
-#include "vdm_proto.h"
-#include "tlmdriver.h"
 
-testbench::testbench(sc_module_name name) {
-    vdm_proto device("device");
-    tlmdriver driver("driver");
+testbench::testbench(sc_module_name name) : device("device")
+                                           ,driver("driver")
+{
     cout << "Greetings from testbench\n";
-    //driver.isocket(device.tsocket);
+    driver.isocket(device.tsocket);
     cout << "Post bind\n";
 }
