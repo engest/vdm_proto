a42aa284f17cbacb3a32d106b62c184bcc89ca3e
Stephen Engelman <softwarerd@gmail.com>
2022-04-15 18:36:41 -0700

    Updated for  later versions

--- test/testbench/env.sh
+++ test/testbench/env.sh
@@ -1,5 +1,5 @@
 export SYSTEMC_VERSION=2.3.1
 export SYSTEMC_HOME=/home/engest/devel/systemc/${SYSTEMC_VERSION}/local
-export LD_LIBRARY_PATH=${SYSTEMC_HOME}/lib-linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${SYSTEMC_HOME}/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/lib/Debug:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/test/tlmdriver/lib/Debug:$LD_LIBRARY_PATH
