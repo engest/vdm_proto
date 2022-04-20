88efd9f7bf6b54eb432393844df71e1b3f5f3a03
Stephen Engelman <softwarerd@gmail.com>
2022-04-20 13:47:38 -0700

    started adding Dox

--- inc/vdm_proto.h
+++ inc/vdm_proto.h
@@ -1,3 +1,13 @@
+/**
+ * @file vdm_proto.h
+ * @author Stephen Engelman
+ * @brief Virtual Device Model Prototype header
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __VDM_PROTO_H__
 #define __VDM_PROTO_H__
 
@@ -10,12 +20,33 @@ using namespace sc_core;
 using namespace tlm_utils;
 using namespace tlm;
 
+/**
+ * @brief Virtual Device Model Prototype
+ * 
+ */
 class vdm_proto : public sc_module
 {
 public:
+    /**
+     * @brief Construct a new vdm proto object
+     * 
+     * @param name SystemC Name string
+     */
     vdm_proto(sc_module_name name);
+
+    /**
+     * @brief TLM2 target callback to handle blocking transactions 
+     * 
+     * @param trans Transaction object 
+     * @param delay Annotated delay for modeling purposes
+     */
     void b_transport(tlm_generic_payload & trans, sc_time & delay);
 
+    /**
+     * @brief TLM2 simple target socket
+     * 
+     * Socket for receiving read/write bus transactions from CPU or other initiating module
+     */
     simple_target_socket<vdm_proto> tsocket;
 
 };
