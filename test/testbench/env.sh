eb1ce2f5465d825845fc6045bd4c66568b64108c
Stephen Engelman <softwarerd@gmail.com>
2022-04-19 20:09:20 -0700

    Added sysc env var checks

--- test/testbench/env.sh
+++ test/testbench/env.sh
@@ -1,5 +1,16 @@
-export SYSTEMC_VERSION=2.3.1
-export SYSTEMC_HOME=/home/engest/devel/systemc/${SYSTEMC_VERSION}/local
-export LD_LIBRARY_PATH=${SYSTEMC_HOME}/lib:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/lib/Debug:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/test/tlmdriver/lib/Debug:$LD_LIBRARY_PATH
+if [[ -z ${SYSTEMC_VERSION} ]]; then
+    echo "Please set SYSTEMC_VERSION environment variable..."
+    echo "IE:"
+    echo "    export SYSTEMC_VERSION=2.3.3"
+    
+else
+    echo "SYSTEMC_VERSION set to ${SYSTEMC_VERSION}"
+    if [[ -z ${SYSTEMC_HOME} ]]; then
+        echo "Please set SYSTEMC_HOME environment variable..."
+    else
+        echo "SYSTEMC_VERSION set to ${SYSTEMC_HOME}"
+        export LD_LIBRARY_PATH=${SYSTEMC_HOME}/lib:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/lib/Debug:$LD_LIBRARY_PATH
+        export LD_LIBRARY_PATH=/home/engest/devel/systemc/work/projects/vdm_proto/test/tlmdriver/lib/Debug:$LD_LIBRARY_PATH
+    fi
+fi
