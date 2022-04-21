3bae130311a4bf06e2f4d9a4f43b6e6f3bda317d
Stephen Engelman <softwarerd@gmail.com>
2022-04-20 17:01:59 -0700

    Adding initial dox

--- test/testbench/inc/testbench.h
+++ test/testbench/inc/testbench.h
@@ -1,3 +1,13 @@
+/**
+ * @file testbench.h
+ * @author Stephen Engelman
+ * @brief Testbench top for the vdm_proto and tlmdriver
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __VDM_TB_H__
 #define __VDM_TB_H__
 
@@ -7,11 +17,32 @@
 #include "vdm_proto.h"
 #include "tlmdriver.h"
 
+/**
+ * @brief Testbench module
+ * 
+ * The top of the "design."
+ * 
+ */
 class testbench : public sc_module
 {
 public:
+    /**
+     * @brief Instance of the device under test vdm_proto
+     * 
+     */
     vdm_proto device;
+
+    /**
+     * @brief Instance of the testing module
+     * 
+     */
     tlmdriver driver;
+
+    /**
+     * @brief Construct a new testbench object
+     * 
+     * @param name SystemC Name string
+     */
     testbench(sc_module_name name);
     
 };
--- test/tlmdriver/inc/tlmdriver.h
+++ test/tlmdriver/inc/tlmdriver.h
@@ -1,3 +1,13 @@
+/**
+ * @file tlmdriver.h
+ * @author Stephen Engelman
+ * @brief TLM2 initiator driver module
+ * @version 0.1
+ * @date 2022-04-20
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef __VDM_DRV_H__
 #define __VDM_DRV_H__
 
@@ -9,13 +19,50 @@ using namespace std;
 using namespace sc_core;
 using namespace tlm_utils;
 using namespace tlm;
+
+/**
+ * @brief TLM2 initiator driver module class
+ * 
+ * This module is intended to be used to initiate transactions to the device under test, the vdm_proto module.
+ * 
+ */
 class tlmdriver : public sc_module
 {
 public:
+
+    /**
+     * @brief Construct a new tlmdriver object
+     * 
+     * @param name SystemC Name string
+     */
     tlmdriver(sc_module_name name);
+
+    /**
+     * @brief TLM2 simple initiator socket
+     * 
+     */
     simple_initiator_socket<tlmdriver> isocket;
+
+    /**
+     * @brief A SystemC event used for triggering an action
+     * 
+     * This is currently not used.
+     * 
+     */
     sc_event trigger;
+
+    /**
+     * @brief Callback from sc_module
+     * 
+     * This is inherited from sc_module class and is a callback for the start of actual simulation running.
+     */
     void start_of_simulation();
+
+    /**
+     * @brief An SC_THREAD function
+     * 
+     * This is used as an SC_THREAD. It will be the main thread of initiating transactions and consuming time.
+     */
     void drive();
 };
 #endif
