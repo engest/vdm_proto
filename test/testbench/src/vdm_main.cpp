bb11814c19c2b8e81b988c5841bf071c69175d7e
Stephen Engelman <softwarerd@gmail.com>
2022-04-27 21:41:15 -0700

    Set sc_start to a fixed time of 100ns

--- test/testbench/src/vdm_main.cpp
+++ test/testbench/src/vdm_main.cpp
@@ -4,7 +4,7 @@ int sc_main(int argc, char * argv[]) {
     // Instance top
     testbench tb("tb");
     cout << "Pre sc_start()\n";
-    sc_core::sc_start();
+    sc_core::sc_start(100, SC_NS);
     cout << "Post sc_start()\n";
     return 0;
 }
